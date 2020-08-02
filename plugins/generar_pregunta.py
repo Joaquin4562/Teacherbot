4fc80d9785b178507406bbb2777839d419f9b734
Joaquin4562 <martinez.hernandez.17087@itsmante.edu.mx>
2020-08-01 21:06:51 -0500

    contador de respuestas agregado

--- plugins/generar_pregunta.py
+++ plugins/generar_pregunta.py
@@ -3,7 +3,7 @@ from os.path import join, dirname, abspath
 def generarPregunta():
     long = obtenerLongitud()
     if long > 0:
-        pregunta = random.randint(1,3)
+        pregunta = random.randint(0,2)
         verbo = random.randint(1,long)
         contador = 1
         file = open (join(dirname(dirname(abspath(__file__))), 'plugins', 'verbosEstudiados.txt'), 'r')
@@ -15,10 +15,10 @@ def generarPregunta():
                 break
             contador+=1    
         file.close()
-        preguntasAElegir = {'1':"Dime la traducción al Inglés de ", '2':"Dime el pasado de ", '3':"Dime el participio de "}
+        preguntasAElegir = {'0':"Dime la traducción al Inglés de ", '1':"Dime el pasado de ", '2':"Dime el participio de "}
         preguntaElegida = preguntasAElegir[str(pregunta)]
         preguntaCompleta = preguntaElegida + verboElegido
-        return 'set_slot preguntas ["{0}","{1}","{2}"]'.format(preguntaElegida, verboElegido, preguntaCompleta)
+        return 'set_slot preguntas ["{0}","{1}","{2}"]'.format(pregunta, verboElegido, preguntaCompleta)
     else:
         return 'say "Aun no has estudiado nada"'   
 #Metodo de apoyo
--- plugins/verificar_respuesta.py
+++ plugins/verificar_respuesta.py
@@ -8,24 +8,10 @@ def verificarRespuesta(verb, pregunta, respuesta):
     for i in range(sheet.nrows):
         valor = sheet.cell_value(i,3)
         if(valor == verb):
-            traduccion = sheet.cell_value(i,0)
-            pasado = sheet.cell_value(i,1)
-            participio = sheet.cell_value(i,2)
-            if pregunta == "Dime la traducción al Inglés de ":
-                if traduccion == respuesta:
-                    return 'say "Respuesta Correcta"'
-                else :
-                    return 'say "Respuesta Incorrecta"'
-
-            if pregunta == "Dime el pasado de ":
-                if pasado == respuesta:
-                    return 'say "Respuesta Correcta"'
-                else :
-                    return 'say "Respuesta Incorrecta"'
-
-            if pregunta == "Dime el participio de ":
-                if participio == respuesta:
-                    return 'say "Respuesta Correcta"'
-                else :
-                    return 'say "Respuesta Incorrecta"'
-    return 'say "No se entendio tu respuesta, puto"'
+            respuestaCorrecta = sheet.cell_value(i, int(pregunta))
+            if respuestaCorrecta == respuesta:
+                return "execute self.slots['cont_correcto']+=1"
+            else:
+                return "execute self.slots['cont_incorrecto']+=1"
+        
+    return 'say "No se entendio tu respuesta"'
